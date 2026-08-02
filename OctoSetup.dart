0c0379659cdabab357aafec37580132ee5c2f09f
brodawgissocool-genza <brodawgissocool@gmail.com>
2026-08-02 15:21:19 -0500

    Create OctoSetup.dart

---
+++ OctoSetup.dart
@@ -0,0 +1,12 @@
+import 'dart:html' as Html;
+
+var Luau, Json, Layout_type;
+
+
+main() {
+  print('Octo.cloud fundemental (USE LUAU AND PYTHON!)');
+  Luau = 'True';
+  Json = 'Dynamic ';
+  Layout_type = Html.window.prompt('Layout typw? Dark/punchy, white/punchy, or Dark/classic, and White/classic', '');
+  print('Using the revision…….');
+}
